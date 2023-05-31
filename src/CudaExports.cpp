cb7a4974149a1a4ad87d589672f0e1fc2ecb59dd
Z James <42901131+zjames12@users.noreply.github.com>
2023-05-31 14:24:32 -0400

    full vecchiaLinv gpu

--- src/CudaExports.cpp
+++ src/CudaExports.cpp
@@ -1,5 +1,9 @@
-#include <Rcpp.h>
+// #include <Rcpp.h>
+#include <RcppArmadillo.h>
+//[[Rcpp::depends(RcppArmadillo)]]
+
 using namespace Rcpp;
+using namespace arma;
 
 extern "C"
 double* vecchia_Linv_gpu_outer(
@@ -22,41 +26,46 @@ double meanC(NumericVector x) {
 }
 
 // [[Rcpp::export]]
-NumericMatrix vecchia_Linv_gpu_isotropic_exponential(
-    NumericVector covparms,
-    NumericMatrix locs,
-    NumericMatrix NNarray) {
+arma::mat vecchia_Linv_gpu_isotropic_exponential(
+    arma::vec covparms,
+    arma::mat locs,
+    arma::mat NNarray) {
     
-    int m = NNarray.ncol();
-    int n = locs.nrow();
-    int nparms = covparms.length();
-    int dim = locs.ncol();
+    int m = NNarray.n_cols;
+    int n = locs.n_rows;
+    int nparms = covparms.n_elem;
+    int dim = locs.n_cols;
 
     double covparmsl[3] = { 0, 0, 0 };
     covparmsl[0] = covparms[0];
     covparmsl[1] = covparms[1];
     covparmsl[2] = covparms[2];
 
-    double* locsl = (double*)malloc(sizeof(double) * n * dim);
-    double* NNarrayl = (double*)malloc(sizeof(double) * n * m);
+    // double* locsl = (double*)malloc(sizeof(double) * n * dim);
+    locs = locs.t();
+    double* locsl = locs.memptr();
+    // double* NNarrayl = (double*)malloc(sizeof(double) * n * m);
+    NNarray = NNarray.t();
+    double* NNarrayl = NNarray.memptr();
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            if (j < dim) {
-                locsl[i * dim + j] = locs(i, j);
-            }
-            NNarrayl[i * m + j] = NNarray(i, j);
-        }
-    }
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < m; j++){
+    //         if (j < dim) {
+    //             // locsl[i * dim + j] = locs(i, j);
+    //         }
+    //         NNarrayl[i * m + j] = NNarray(i, j);
+    //     }
+    // }
 
     double* Linvl = vecchia_Linv_gpu_outer(covparmsl, locsl, NNarrayl, n, m, dim);
 
-    NumericMatrix Linv( n , m );
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            Linv(i,j) = Linvl[i * m + j];
-        }
-    }
-    return Linv;
+    // NumericMatrix Linv( n , m );
+    // arma::mat Linv = arma::mat(n, m);
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < m; j++){
+    //         Linv(i,j) = Linvl[i * m + j];
+    //     }
+    // }
+    arma::mat Linv = arma::mat(&Linvl[0], m, n, false);
+    return Linv.t();
 }
