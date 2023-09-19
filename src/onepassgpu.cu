512dd8e33ad05149f2dcdbe62eb97d5324d04fee
Zachary James <zj37@g2-login-02.coecis.cornell.edu>
2023-09-19 15:36:17 -0400

    vecchia_Linv moved to registers

--- src/onepassgpu.cu
+++ src/onepassgpu.cu
@@ -190,14 +190,14 @@ __global__ void call_vecchia_Linv_gpu(double* locs, double* NNarray,
     if (i >= n || i < start_ind) {
         return;
     }
-    /*__shared__ double ls[16 * 21 * 2];
-    __shared__ double cov[16 * 21 * 21];*/
+    double ls[31 * 2];
+    double cov[31 * 31];
     
     //clock_t start = clock();
     for (int j = 0; j < m; j++) {
         for (int k = 0; k < dim; k++) {
-            locs_scaled[i * m * dim + (m - 1 - j) * dim + k] = ( locs[(static_cast<int>(NNarray[i * m + j]) - 1) * dim + k] ) / range;
-            //ls[threadIdx.x * m * dim + (m - 1 -j) * dim + k] = locs[(static_cast<int>(NNarray[i * m + j]) - 1) * dim + k] / range;
+            // locs_scaled[i * m * dim + (m - 1 - j) * dim + k] = ( locs[(static_cast<int>(NNarray[i * m + j]) - 1) * dim + k] ) / range;
+            ls[(m - 1 -j) * dim + k] = locs[(static_cast<int>(NNarray[i * m + j]) - 1) * dim + k] / range;
         }
     }
     //clock_t end = clock();
@@ -208,20 +208,21 @@ __global__ void call_vecchia_Linv_gpu(double* locs, double* NNarray,
     for (int i1 = 0; i1 < m; i1++) {
         for (int i2 = 0; i2 <= i1; i2++) {
             // calculate distance
-            double d = hypot(locs_scaled[i * m * dim + i1 * dim ]- locs_scaled[i * m * dim + i2 * dim], 
-                locs_scaled[i * m * dim + i1 * dim + 1] - locs_scaled[i * m * dim + i2 * dim + 1]);
-            /*double d = hypot(ls[threadIdx.x * m * dim + i1 * dim] - ls[threadIdx.x * m * dim + i2 * dim],
-                ls[threadIdx.x * m * dim + i1 * dim + 1] - ls[threadIdx.x * m * dim + i2 * dim + 1]);*/
+            //double d = hypot(locs_scaled[i * m * dim + i1 * dim ]- locs_scaled[i * m * dim + i2 * dim], 
+              //  locs_scaled[i * m * dim + i1 * dim + 1] - locs_scaled[i * m * dim + i2 * dim + 1]);
+            
+			double d = hypot(ls[i1 * dim] - ls[i2 * dim],
+                ls[i1 * dim + 1] - ls[i2 * dim + 1]);
             // calculate covariance
             if (i1 == i2) {
-                covmat[i * m * m + i2 * m + i1] = variance * (expf(-d) + nugget);
-                //cov[threadIdx.x * m * m + i2 * m + i1] = variance * (exp(-d) + nugget);
+                // covmat[i * m * m + i2 * m + i1] = variance * (expf(-d) + nugget);
+                cov[i2 * m + i1] = variance * (exp(-d) + nugget);
             }
             else {
-                covmat[i * m * m + i2 * m + i1] = variance * expf(-d);
-                /*cov[threadIdx.x * m * m + i2 * m + i1] = variance * exp(-d);
-                cov[threadIdx.x * m * m + i1 * m + i2] = cov[threadIdx.x * m * m + i2 * m + i1];*/
-                covmat[i * m * m + i1 * m + i2] = covmat[i * m * m + i2 * m + i1];
+                // covmat[i * m * m + i2 * m + i1] = variance * expf(-d);
+                cov[i2 * m + i1] = variance * exp(-d);
+                cov[i1 * m + i2] = cov[i2 * m + i1];
+                // covmat[i * m * m + i1 * m + i2] = covmat[i * m * m + i2 * m + i1];
             }
         }
     }
@@ -238,23 +239,23 @@ __global__ void call_vecchia_Linv_gpu(double* locs, double* NNarray,
         diff = 0;
         for (k = 0; k < m; k++) {
             if (k < q) {
-                //temp = cov[threadIdx.x * m * m + k * m + q];
-                temp = covmat[i * m * m + k * m + q];
+                temp = cov[k * m + q];
+                // temp = covmat[i * m * m + k * m + q];
                 diff -= temp * temp;
             }
             else if (k == q) {
-                //cov[threadIdx.x * m * m + q * m + q] = sqrt(cov[threadIdx.x * m * m + q * m + q] + diff);
-                covmat[i * m * m + q * m + q] = sqrt(covmat[i * m * m + q * m + q] + diff);;
+                cov[q * m + q] = sqrt(cov[q * m + q] + diff);
+                //covmat[i * m * m + q * m + q] = sqrt(covmat[i * m * m + q * m + q] + diff);;
                 diff = 0;
             }
             else {
                 diff = 0;
                 for (int p = 0; p < q; p++) {
-                    diff += covmat[i * m * m + p * m + q] * covmat[i * m * m + p * m + k];
-                    //diff += cov[threadIdx.x * m * m + p * m + q] * cov[threadIdx.x * m * m + p * m + k];
+                    //diff += covmat[i * m * m + p * m + q] * covmat[i * m * m + p * m + k];
+                    diff += cov[p * m + q] * cov[p * m + k];
                 }
-                covmat[i * m * m + q * m + k] = (covmat[i * m * m + q * m + k] - diff ) / covmat[i * m * m + q * m + q];
-                //cov[threadIdx.x * m * m + q * m + k] = (cov[threadIdx.x * m * m + q * m + k] - diff) / cov[threadIdx.x * m * m + q * m + q];
+                //covmat[i * m * m + q * m + k] = (covmat[i * m * m + q * m + k] - diff ) / covmat[i * m * m + q * m + q];
+                cov[q * m + k] = (cov[q * m + k] - diff) / cov[q * m + q];
             }
         }
 
@@ -271,11 +272,11 @@ __global__ void call_vecchia_Linv_gpu(double* locs, double* NNarray,
     for (int q = m - 1; q >= 0; q--) {
         double sum = 0.0;
         for (int j = q + 1; j < m; j++) {
-            sum += covmat[i * m * m + q * m + j] * NNarray[i * m + m - 1 - j];
-            //sum += cov[threadIdx.x * m * m + q * m + j] * NNarray[i * m + m - 1 - j];
+            //sum += covmat[i * m * m + q * m + j] * NNarray[i * m + m - 1 - j];
+            sum += cov[q * m + j] * NNarray[i * m + m - 1 - j];
         }
-        NNarray[i * m + m - 1 - q] = (b - sum) / covmat[i * m * m + q * m + q];
-        //NNarray[i * m + m - 1 - q] = (b - sum) / cov[threadIdx.x * m * m + q * m + q];
+        //NNarray[i * m + m - 1 - q] = (b - sum) / covmat[i * m * m + q * m + q];
+        NNarray[i * m + m - 1 - q] = (b - sum) / cov[q * m + q];
         b = 0;
     }
     clock_t end = clock();
